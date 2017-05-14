9f0cf22a8937bcdadff3287087e0b9261711d955
Glenn Harmon <gharmon@covermymeds.com>
2017-05-14 17:41:54 -0400

    modified db name

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/Brandon'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/cradletocrayon'
 db = SQLAlchemy(app)
 
 # Create our database model
@@ -26,7 +26,7 @@ def index():
     return render_template('index.html')
 
 # Save e-mail to database and send to success page
-@app.route('/Brandon', methods=['POST'])
+@app.route('/craydletocrayon', methods=['POST'])
 def prereg():
     email = None
     username = None
