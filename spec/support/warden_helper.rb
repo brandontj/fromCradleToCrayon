66c62fef5102d30c81c3e7a1e92434c004af4452
Glenn Harmon <gharmon@covermymeds.com>
2018-06-07 16:09:02 -0400

    spec factories and login test

---
+++ spec/support/warden_helper.rb
@@ -0,0 +1,12 @@
+module WardenHelper
+  RSpec.configure do |config|
+    config.include Warden::Test::Helpers, type: :feature
+    config.include self, type: :feature
+  end
+
+  def user_logged_in(user = create(:user))
+    login_as(user, :scope => :user)
+    visit '/'
+  end
+end
+
