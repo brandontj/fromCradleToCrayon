d88be2015ef3a081e8e3a1ed6e4113ab4a8032d9
Glenn Harmon <gharmon@covermymeds.com>
2019-04-01 10:03:08 -0400

    wip

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,19 +1,25 @@
 class ExpensesController < ApplicationController
   before_action :authenticate_user!
-  attr_reader :user_id, :expenses
 
   def index
-    @user = user
-    @expenses = IndexExpenseOperation.new(user).results
+    @expenses = IndexExpenseOperation.new(user_id).results
     respond_to do |format|
       format.html
       format.json { render json: { expenses: @expenses }, status: 200 }
     end
   end
 
+  def new
+    @expense = Expense.new
+  end
+
+  def create
+
+  end
+
   private
 
-  def user
+  def user_id
     user_id = current_user.id
   end
 end
